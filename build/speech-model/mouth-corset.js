41986b1f35ad0b0180cd22bfbd3ffb1242981085
Dwscdv3 <dwscdv3@hotmail.com>
2022-12-08 12:25:12 +0800

    remove: built files

--- build/speech-model/mouth-corset.js
+++
@@ -1,8 +0,0 @@
-import pinyin from '../pinyin.js';
-const handlers = [{
-        predicate: char => char.charCodeAt(0) >= 0x4E00 && char.charCodeAt(0) < 0xA000,
-        func: text => pinyin(text).map((py) => ' ¯ˊˇˋ'[pinyin.parts(py).tone] ?? '¯').join(''),
-    }];
-export { handlers };
-export default handlers;
-//# sourceMappingURL=mouth-corset.js.map
--- build/speech-model/robot.js
+++
@@ -1,8 +0,0 @@
-import pinyin from '../pinyin.js';
-const handlers = [{
-        predicate: char => true,
-        func: text => pinyin(text).join(' '),
-    }];
-export { handlers };
-export default handlers;
-//# sourceMappingURL=robot.js.map
