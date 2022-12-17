ade1701a55b3c20a09ffcb4003e2f494b5e3e5a7
Dwscdv3 <dwscdv3@hotmail.com>
2022-12-17 10:53:27 +0800

    add: lib and type definitions

---
+++ src/lib/bondage-club-garbler.d.ts
@@ -0,0 +1,4 @@
+export function SpeechGarbleByGagLevel(
+    GagEffect: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7,
+    CD: string,
+): string;
---
+++ src/lib/to-zh-digit.d.ts
@@ -0,0 +1,1 @@
+export function toZhDigit(digit: number | string): string;
